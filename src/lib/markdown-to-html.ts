ceae294914fcf77a5bd68010b2a34890778f8a3b
Muhammad Rifqi Priyo Susanto <rifqipriyo@gmail.com>
2026-05-25 10:20:00 +0700

    [FE] Update markdownToHtml function

--- src/lib/markdown-to-html.ts
+++ src/lib/markdown-to-html.ts
@@ -1,20 +1,5 @@
 import { marked } from "marked";
 
-export function markdownToHtml(markdown: string) {
-  return marked.parse(markdown, { async: false });
+export function markdownToHtml(markdown: string): string {
+  return marked.parse(markdown, { async: false }) as string;
 }
-
-// export async function parsedMarkdownToHtml(markdown: string): Promise<string> {
-//   const html = await marked.parse(markdown);
-//   return DOMPurify.sanitize(html);
-// }
-
-// export function useMarkdown(markdown: string) {
-//   const [html, setHtml] = useState("");
-
-//   useEffect(() => {
-//     parsedMarkdownToHtml(markdown).then(setHtml);
-//   }, [markdown]);
-
-//   return html;
-// }
